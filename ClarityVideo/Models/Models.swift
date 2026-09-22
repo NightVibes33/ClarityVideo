94758d8d83b2ce361114fb177322f0e10e680b4a
Zyn <bobbytatum999@gmail.com>
2026-09-22 12:03:56 -0500

    Centralize storage-efficient HEVC bitrate ranges

--- ClarityVideo/Models/Models.swift
+++ ClarityVideo/Models/Models.swift
@@ -144,6 +144,21 @@ extension ExportConfiguration {
     }
 }
 
+extension ExportConfiguration {
+    var exportBitrateOptionsMbps: [Int] {
+        switch resolution {
+        case .uhd4K: [24, 40, 60]
+        case .uhd8K: [45, 70, 100]
+        }
+    }
+
+    mutating func clampBitrateToSupportedRange() {
+        guard let minimum = exportBitrateOptionsMbps.first,
+              let maximum = exportBitrateOptionsMbps.last else { return }
+        bitrateMbps = min(max(bitrateMbps, minimum), maximum)
+    }
+}
+
 struct DeviceEnhancementCapabilities: Codable, Equatable, Sendable {
     var fullSuperResolutionAvailable = false
     var lowLatencySuperResolutionAvailable = false
